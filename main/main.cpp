19b4f71536fb7faad51736e307d93fb191886e38
Mike Abbott <furrysalamander@gmail.com>
2023-10-28 20:11:18 -0600

    test DACGraphics

--- main/main.cpp
+++ main/main.cpp
@@ -1,50 +1,41 @@
-#include <math.h>
-#include <driver/dac_cosine.h>
-
-#include <vector>
-// #include <thread>
-// #include <chrono>
-
-extern "C" {
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-}
 
-// using namespace std::chrono_literals;
+#include <rtc_wdt.h>
+#include <esp_task_wdt.h>
+#include <driver/dac_cosine.h>
+#include <math.h>
+#include <vector>
+#include <array>
+#include <thread>
+
+#include "bitluni/DACGraphics.h"
+#include "bitluni/Mesh.h"
+
+void blit(const std::vector<std::array<uint8_t, 4>>& lines) {
+    // May want to replace this with the stanrdard library DAC calls at some point.
+    // it's currently using the oudated driver API.  Maybe the new one isn't as slow.
+    // (thus eliminating the need for the fastdac code)
+    DACGraphics::begin(); 
+    for (const auto &line: lines) {
+        DACGraphics::line(line[0], line[1], line[2], line[3]);
+    }
+    DACGraphics::end();
+}
 
 extern "C" void app_main(void)
 {
-    dac_cosine_handle_t x_channel_handle;
-    dac_cosine_config_t x_config = {
-        .chan_id = DAC_CHAN_0,
-        .freq_hz = 1000, // It will be covered by 8000 in the latter configuration
-        .clk_src = DAC_COSINE_CLK_SRC_DEFAULT,
-        .atten = DAC_COSINE_ATTEN_DEFAULT,
-        .phase = DAC_COSINE_PHASE_0,
-        .offset = 0,
+    DACGraphics::init();
+    std::vector<std::array<uint8_t, 4>> square {
+        {16,16,16,240},
+        {16,240,240,240},
+        {240,240,240,16},
+        {240,16,16,16},
     };
-    x_config.flags.force_set_freq = false;
 
-    dac_cosine_handle_t y_channel_handle;
-    dac_cosine_config_t y_config = {
-        .chan_id = DAC_CHAN_1,
-        .freq_hz = 8000,
-        .clk_src = DAC_COSINE_CLK_SRC_DEFAULT,
-        .atten = DAC_COSINE_ATTEN_DB_6,
-        .phase = DAC_COSINE_PHASE_180,
-        .offset = 0,
-    };
-    y_config.flags.force_set_freq = true;
-
-
-    ESP_ERROR_CHECK(dac_cosine_new_channel(&x_config, &x_channel_handle));
-    ESP_ERROR_CHECK(dac_cosine_new_channel(&y_config, &y_channel_handle));
-
-    ESP_ERROR_CHECK(dac_cosine_start(x_channel_handle));
-    ESP_ERROR_CHECK(dac_cosine_start(y_channel_handle));
-
-    while (1) {
-        // std::this_thread::sleep_for(1ms);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+    while (true) {
+        for (int i = 0; i < 1e2; i++)
+            blit(square);
+        vTaskDelay(1);
     }
 }
