947060a84d0d0dd7442a23d7df5b82382f67c08d
Mike Abbott <furrysalamander@gmail.com>
2023-11-03 14:58:57 -0600

    asdf

--- main/dacGraphics.cpp
+++ main/dacGraphics.cpp
@@ -7,8 +7,6 @@ namespace dac {
     dac_continuous_handle_t dac_output_handle = nullptr;
 
     void update_buffer(std::unique_ptr<std::vector<uint8_t>> buffer) {
-        // assert(data->size() < 4096);
-
         if (dac_output_handle != nullptr) {
             ESP_ERROR_CHECK(dac_continuous_del_channels(dac_output_handle));
         }
@@ -22,7 +20,7 @@ namespace dac {
         .desc_num = 8,
         // I think maybe this is just the max size?  If so, we don't need to re-init the dma stuff every time.
         .buf_size = 4092, 
-        .freq_hz = 1000000, // frequency at which data is written to the output
+        .freq_hz = 60000, // frequency at which data is written to the output
         .offset = 0,
         .clk_src = DAC_DIGI_CLK_SRC_DEFAULT,
         .chan_mode = DAC_CHANNEL_MODE_ALTER,
@@ -32,7 +30,5 @@ namespace dac {
     ESP_ERROR_CHECK(dac_continuous_enable(dac_output_handle));
     ESP_ERROR_CHECK(dac_continuous_write_cyclically(dac_output_handle, data->data(), data->size(), NULL));
     }
-
-    // add_line std::array<uint8_t, 4>
 }
 
