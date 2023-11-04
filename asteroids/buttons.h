42ea203910409c3215bdc3df3a41a1a9e3cf321c
caleb.hensley <caleb.hensley@lvt.com>
2023-11-03 21:43:18 -0600

    further cleanup, add buttons file for future

---
+++ asteroids/buttons.h
@@ -0,0 +1,17 @@
+/*********************************************
+ * All things specific to the vectorscope
+ **********************************************/
+
+#ifndef VECTORSCOPE_H
+#define VECTORSCOPE_H
+
+enum Button {
+   STICK_UP,
+   STICK_DOWN,
+   STICK_RIGHT,
+   STICK_LEFT,
+   LEVEL,
+   RANGE
+};
+
+#endif // UI_INTERFACE_H
